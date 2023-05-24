aa8a0a90e1baadaa1e392ffb8d377600a1bb8995
Guslizen Effayed <guslizenefayed@gmail.com>
2023-05-24 21:50:46 +0700

    membuat kerangka database, memasukkan dalam list installed apps dan melakukan makemigrations

--- mysiteprojek/polls/models.py
+++ mysiteprojek/polls/models.py
@@ -5,8 +5,8 @@ class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('Date Published')
 
+
 class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
