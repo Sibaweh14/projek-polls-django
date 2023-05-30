d72cd7bd6a95265148a15195715e9666fa9e4a37
guslizeneffayed <guslizenefayed@gmail.com>
2023-05-31 01:08:01 +0700

    menambahkan fungsi detail, result, vote

--- mysiteprojek/polls/views.py
+++ mysiteprojek/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse('halo nama saya guslizen effayed')
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s." % question_id)
+
+def result(request, question_id):
+	response = "you're looking at ther result of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
