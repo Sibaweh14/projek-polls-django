e1ffae7b28043a675d78c352011ed41049f7f08b
guslizeneffayed <guslizenefayed@gmail.com>
2023-06-04 00:14:35 +0700

    menggunakan fun render pada httprsponse

--- mysiteprojek/polls/views.py
+++ mysiteprojek/polls/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
+from django.http import Http404
 # Create your views here.
 def index(request):
-	return HttpResponse('halo nama saya guslizen effayed, saya adalah seorang programmer yang sukses')
-
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	context = {
+		"latest_question_list": latest_question_list,
+	}
+	return render(request,"polls/index.html", context)
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(id=question_id)
+	except Question.DoesNotExist:
+		raise Http404('Question doesnt exist')
+	return render(request, 'polls/detail.html', {'question':question})
+
 
 def result(request, question_id):
 	response = "you're looking at ther result of question %s."
